a2bcbab84e5ef06d7a9637abfc7ce21617a49b92
anlu408 <71849385+anlu408@users.noreply.github.com>
2023-06-08 14:13:14 -0700

    Navbar Sign in button created + added logo

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -10,6 +10,9 @@ const AuthPage:React.FC<AuthPageProps> = () => {
     return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
         <div className="max-w-7x1 mx-auto">
             <Navbar />
+            <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+                <img src='/hero.png' alt='Hero.img'/>
+            </div>
         </div>
         </div>
 }
